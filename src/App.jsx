2463030c02358e20771abf44876bed902f9b2d47
Miqueas <miqueasgigena18@gmail.com>
2025-03-03 23:52:52 -0300

    git feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
+import personService from './services/persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+
+  useEffect(() => {
+    personService.getAll().then((initialPersons) => {
+      if (initialPersons) {
+        setPersons(initialPersons)
+      } else {
+        console.error('La respuesta del servidor no es un array:', initialPersons)
+        setPersons([])
+      }
+    }).catch(error => {
+      console.error('Error al obtener Personas:', error)
+      setPersons([]) 
+    })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      phone: newPhone,
+    }
+
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewPhone('')
+    })
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handlePhoneChange = (event) => setNewPhone(event.target.value)
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={addPerson}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          Phone <input value={newPhone} onChange={handlePhoneChange} />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key = {person.id}>{person.name} {person.phone}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
